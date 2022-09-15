be8f95974e6102dc962cf4d471aca73f964b2535
saicharan-125 <113087281+saicharan-125@users.noreply.github.com>
2022-09-15 01:39:11 -0700

    Add files via upload

---
+++ BODMAS.PYTHON.py
@@ -0,0 +1,22 @@
+x=input("enter the first value :")
+y=input("enter the second value :")
+if x.isnumeric() and y.isnumeric():
+    a=int(x)
+    b=int(y)    
+    c=input("enter any opeartor")
+    
+    if c=='+':
+        print(a+b)
+    if c=='-':
+        print(a-b)
+    if c=='*':
+        print(a*b)
+    if c=='/':
+        print(a/b)
+elif x.isalpha() and y.isalpha():
+    print("both are alphabets")
+    
+else:
+    print("both are not same")
+
+
